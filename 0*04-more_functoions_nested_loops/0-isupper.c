2b666723dccbe14c68271009f2664849ec477b7d
Nandiehakuna <99404405+Nandiehakuna@users.noreply.github.com>
2022-03-17 10:22:48 -0700

    Update and rename README.md to 0*04-more_functoions_nested_loops/0-isupper.c

---
+++ 0*04-more_functoions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+  
+{ 
+  char ch;
+  printf("enter any character: ");
+  scanf("%c", &ch);
+  
+  
+  
+  if(ch >='A' && <='Z'
+     {
+       printf("'%c' is uppercase alphabet.",ch);
+     }
+     
+     elseif (ch >='a' && <='z'
+         
+         {
+           printf("'%C' is lowercase. ,ch);
+                  }
+                  
+      else 
+                  {
+                    printf("'%c' is not an alphabet. , ch );
+                           }
+         return 0
+                           }                          
+                  
